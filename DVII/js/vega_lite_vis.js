52260160ccca174edde9ba02e45feb53e023fbc4
sakinahamid <38247622+sakinahamid@users.noreply.github.com>
2021-10-17 14:09:14 +0800

    edited data

--- DVII/js/vega_lite_vis.js
+++ DVII/js/vega_lite_vis.js
@@ -1,44 +1,44 @@
 var vg_1 = "js/emissionsByGas.vg.json";
-  vegaEmbed('#donut_chart', vg_1).then(function(result) {
+  vegaEmbed('#donut_chart', vg_1, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
 
 var vg_2 = "js/bumpChart.vg.json";
-vegaEmbed('#bump_chart', vg_2).then(function(result) {
+vegaEmbed('#bump_chart', vg_2, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "js/averageCo2BarChart.vg.json";
-vegaEmbed('#bar_chart', vg_3).then(function(result) {
+vegaEmbed('#bar_chart', vg_3, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_4 = "js/regionCo2.vg.json";
-vegaEmbed('#hconcat_co2', vg_4).then(function(result) {
+vegaEmbed('#hconcat_co2', vg_4, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_5 = "js/areaChartGlobalSeaLevel.vg.json";
-vegaEmbed('#area_chart_sea', vg_5).then(function(result) {
+vegaEmbed('#area_chart_sea', vg_5, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_6 = "js/tempChangeAreaChart.vg.json";
-vegaEmbed('#area_chart_temp', vg_6).then(function(result) {
+vegaEmbed('#area_chart_temp', vg_6, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_7 = "js/choroplethMap.vg.json";
-vegaEmbed('#choropleth_map', vg_7).then(function(result) {
+vegaEmbed('#choropleth_map', vg_7, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_8 = "js/choroplethMultiple.vg.json";
-vegaEmbed('#choropleth_multiple', vg_8).then(function(result) {
+vegaEmbed('#choropleth_multiple', vg_8, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_9 = "js/tempHeatMap.vg.json";
-vegaEmbed('#heat_map', vg_9).then(function(result) {
+vegaEmbed('#heat_map', vg_9, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
